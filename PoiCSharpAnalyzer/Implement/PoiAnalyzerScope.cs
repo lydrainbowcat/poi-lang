3f3d8428dee56d1777f3cbaf8a27f80657e3957d
MusicCraftor <liyh98@126.com>
2015-11-18 22:18:01 +0900

    PoiCSharpAnalyzerImplement: add variable declaration implement
    Signed-off-by: MusicCraftor <liyh98@126.com>

---
+++ PoiCSharpAnalyzer/Implement/PoiAnalyzerScope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PoiLanguage
+{
+    class PoiAnalyzerScope
+    {
+        private String scopeIdentifier;
+
+        public PoiAnalyzerScope(String identifier)
+        {
+            scopeIdentifier = identifier;
+        }
+
+        public String GetScope()
+        {
+            return scopeIdentifier;
+        }
+    }
+}
---
+++ PoiCSharpAnalyzer/Implement/PoiVariable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace PoiLanguage
+{
+    enum PoiVariableAccesser
+    {
+        Get = 0,
+        Set = 1
+    }
+    class PoiVariable
+    {
+        public String Identifier {get; set;}
+        public PoiAnalyzerScope Scope {get; set;}
+
+        public PoiVariable()
+        {
+            Identifier = "";
+        }
+
+        public PoiVariable(String identifier, PoiAnalyzerScope scope)
+        {
+            Identifier = identifier;
+            Scope = scope;
+        }
+    }
+}
