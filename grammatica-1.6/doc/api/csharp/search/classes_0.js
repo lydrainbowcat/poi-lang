3ebf00789833d17db0b1ebae60e5d319e607ef90
MusicCraftor <liyh98@126.com>
2016-01-07 21:34:00 +0900

    Removed grammatica-1.6 documents from project
    Signed-off-by: MusicCraftor <liyh98@126.com>

--- grammatica-1.6/doc/api/csharp/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['analyzer',['Analyzer',['../class_per_cederberg_1_1_grammatica_1_1_runtime_1_1_analyzer.html',1,'PerCederberg::Grammatica::Runtime']]]
-];
--- grammatica-1.6/doc/api/csharp/search/classes_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['node',['Node',['../class_per_cederberg_1_1_grammatica_1_1_runtime_1_1_node.html',1,'PerCederberg::Grammatica::Runtime']]]
-];
